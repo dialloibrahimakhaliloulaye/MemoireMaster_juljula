174f43e994d8656083652c37e1f12ec3aab07274
dialloibrahimakhaliloulaye <dialloibrahimakhaliloulaye@gmail.com>
2021-05-31 11:28:37 +0000

    my first commit

---
+++ src/main/java/com/juljula/JuljulaApplication.java
@@ -0,0 +1,13 @@
+package com.juljula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuljulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuljulaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juljula/JuljulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juljula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuljulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
