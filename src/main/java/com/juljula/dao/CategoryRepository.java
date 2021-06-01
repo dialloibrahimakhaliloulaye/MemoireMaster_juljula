c629b478049780bd4146525123d95b7b4a74b6bd
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-06-01 00:46:39 +0000

    permission avec crosOrigin

--- src/main/java/com/juljula/dao/CategoryRepository.java
+++ src/main/java/com/juljula/dao/CategoryRepository.java
@@ -4,7 +4,9 @@ import com.juljula.entities.Category;
 import com.juljula.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 @RepositoryRestResource
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
